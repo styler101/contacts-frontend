62160c64c0e46df4d4d783cb564320a2a806d0aa
Lucas <lucas.santos@mundowap.com.br>
2022-10-10 17:50:00 -0300

    feat: create Header componente

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './assets/styles/global'
-import themes from './assets/theme/index'
+import * as S from './styles'
+import Header from '../Header'
+import GlobalStyles from '../../assets/styles/global'
+import themes from '../../assets/theme'
 
 function App() {
   return (
     <ThemeProvider theme={themes.light}>
-      <h1> MyContacts</h1>
+      <S.Container>
+        <Header />
+      </S.Container>
       <GlobalStyles />
     </ThemeProvider>
   )
