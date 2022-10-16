93a448a675ee9c12218220c026eb43db71a856a1
Lucas <lucas.santos@mundowap.com.br>
2022-10-15 21:34:11 -0300

    feat: get data from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,65 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+import Loader from '../../components/Loader'
+
 import arrow from '../../assets/img/svg/arrow.svg'
 import editIcon from '../../assets/img/svg/edit.svg'
 import trashIcon from '../../assets/img/svg/trash.svg'
 
+import {formatPhone} from '../../utils/Formaters'
+
 
 import * as S from './styles'
 
+// SameOriginPolicy(SOP)  -> Politica de mesma origem só existem dentro de navegadores é quando fazendo a requisão para o mesmo endereço só funciona com funções de requisições com javascript.
+// Origin -> é a nossa url conjunto de protolocolo, dominio e porta.
+
+// CORS -> Cross-origin ressource sharing(compartilhamento de recursos entre origens cruzadas) entre saída e destino.
+
+// Requisições de Perfliet
 const Home = () => {
   const [ search, setSearch] = React.useState('')
+  const [ loading, setLoading ]= React.useState(true)
+  const [data, setData] = React.useState([])
+
+  React.useEffect(() =>{
+    fetch('http://localhost:3001/contacts')
+      .then((response) => {
+        setLoading(true)
+        return response.json()
+      })
+      .then((payload) => {
+        setData(payload)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.log(e)
+        setLoading(false)
+      });
+  },[])
+
+
+
   return (
    <React.Fragment>
+     <Loader loading={loading} />
 
      <S.InputSearchContainer>
-       <input type='text' placeholder='Pesquisar pelo nome'  value={search} onChange={(e) => setSearch((e.target.value))}/>
+       <input
+         type='text'
+         placeholder='Pesquisar pelo nome'
+         value={search}
+         onChange={(e) => setSearch((e.target.value))}/>
      </S.InputSearchContainer>
+
     <S.Container>
+
       <S.Header>
         <strong> 3 Contatos </strong>
         <Link to="/new"> Novo Contato </Link>
       </S.Header>
+
       <S.ListContainer>
         <header>
           <button type="button" className='sort-button'>
@@ -28,66 +68,34 @@ const Home = () => {
           </button>
         </header>
       </S.ListContainer>
-      <S.Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong> Lucas Santos </strong>
-            <small className="category"> linkedin </small>
-          </div>
-          <span> lucas@santos.gmail.com </span>
-          <span> (31)919191-1919</span>
-        </div>
-        <div className="actions">
-          <Link to="/edit/3">
-            <img src={editIcon}  alt="edit icon"/>
-          </Link>
-          <button>
-            <img src={trashIcon} alt="trash icon"/>
-          </button>
-        </div>
 
-      </S.Card>
-      <S.Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong> Lucas Santos </strong>
-            <small className="category"> linkedin </small>
+      {data.map((contact) => (
+        <S.Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong> {contact.name} </strong>
+              <small className="category"> {contact.category_name} </small>
+            </div>
+            <span> {contact.email} </span>
+            <span> {formatPhone(contact.phone)}</span>
           </div>
-          <span> lucas@santos.gmail.com </span>
-          <span> (31)919191-1919</span>
-        </div>
-        <div className="actions">
-          <a href="#">
-            <img src={editIcon}  alt="edit icon"/>
-          </a>
-          <button>
-            <img src={trashIcon} alt="trash icon"/>
-          </button>
-        </div>
-
-      </S.Card>
-      <S.Card>
-        <div className="info">
-          <div className="contact-name">
-            <strong> Lucas Santos </strong>
-            <small className="category"> linkedin </small>
+          <div className="actions">
+            <Link to="/edit/3">
+              <img src={editIcon}  alt="edit icon"/>
+            </Link>
+            <button>
+              <img src={trashIcon} alt="trash icon"/>
+            </button>
           </div>
-          <span> lucas@santos.gmail.com </span>
-          <span> (31)919191-1919</span>
-        </div>
-        <div className="actions">
-          <a href="#">
-            <img src={editIcon}  alt="edit icon"/>
-          </a>
-          <button>
-            <img src={trashIcon} alt="trash icon"/>
-          </button>
-        </div>
 
-      </S.Card>
+        </S.Card>
+
+      ))}
     </S.Container>
    </React.Fragment>
   )
 }
+
+
 
 export default Home
