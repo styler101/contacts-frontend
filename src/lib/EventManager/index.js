dbde0332237574685d8e9305010bea58780e2313
Lucas <lucas.santos@mundowap.com.br>
2022-10-29 12:13:12 -0300

    refactor: change object key to acess value to a Map object

--- src/lib/EventManager/index.js
+++ src/lib/EventManager/index.js
@@ -1,45 +1,30 @@
 export default class EventManager{
 
   constructor() {
-    this.listeners = {}
+    this.listeners = new Map();
   }
   // Quando acontecer algo
   on(eventName, eventFunction){
-    if(!this.listeners[eventName]) {
-      this.listeners[eventName] = []
+    // verifica se o objeto possui essa chave
+    if(!this.listeners.has(eventName)){
+      // seta uma propriedade para um valor.
+      this.listeners.set(eventName, [])
     }
-    this.listeners[eventName].push(eventFunction)
+    this.listeners.get(eventName).push(eventFunction)
   }
   // Função que irá disparar o evento
   emit(eventName, eventPayload){
-    if(!this.listeners[eventName]) return;
-    this.listeners[eventName].forEach((listerners) => {
-      listerners(eventPayload)
-    })
+    if(!this.listeners.get(eventName)) return
+    this.listeners.get(eventName).forEach((listener) => { listener(eventPayload) })
   }
 
   removeListener(eventName, listenerToRemove){
-    const listeners = this.listeners[eventName]
+    const listeners = this.listeners.get(eventName)
     if(!listeners) return;
     // Removendo a função que precisa ser removida.
     // Quando vamos remover funções precisamos remover eles com base no endereço de memória onde ela foi criada.
     const filteredListeners = listeners.filter((listener) => listener !== listenerToRemove);
-    this.listeners[eventName] = filteredListeners
+    this.listeners.set(eventName, filteredListeners)
   }
 }
 
-const toastEventManager = new EventManager();
-
-function addToast1(payload){
-  console.log('addToast Listener 1', payload)
-}
-function addToast2(payload){
-  console.log('addToast Listener 2', payload)
-}
-
-toastEventManager.on('addtoast', addToast1)
-toastEventManager.on('addtoast', addToast2)
-toastEventManager.emit('addtoast', addToast2)
-
-toastEventManager.removeListener('addToast', 'depois de remover.')
-console.log(toastEventManager)
